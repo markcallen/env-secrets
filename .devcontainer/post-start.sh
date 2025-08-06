ab28ed12d578de6fe2c314b1d571ea4472c40f48
Mark C Allen <mark@markcallen.com>
2025-08-06 22:37:40 +0000

    Adding to make it easier to debug in a devcontainer

---
+++ .devcontainer/post-start.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install AWS CLI v2
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip -q awscliv2.zip
+sudo ./aws/install
+rm -rf aws awscliv2.zip
